575660ed0ef91c35a2ce2986251c258277f7945d
Farhan_nr <farhannr75@gmail.com>
2024-07-16 20:16:46 +0700

    Distrik Web

---
+++ .history/src/components/index_20240706142456.js
@@ -0,0 +1,15 @@
+import Button from "./Button";
+import Nav from "./Nav";
+import PopularProductCard from "./PopularProductCard";
+import ServiceCard from "./ServiceCard";
+import ReviewCard from "./ReviewCard";
+import TshirtCard from "./TshirtCard";
+
+export {
+    Button,
+    Nav,
+    PopularProductCard,
+    ServiceCard,
+    ReviewCard,
+    TshirtCard,
+}
---
+++ .history/src/constants/index_20240706164150.js
@@ -0,0 +1,155 @@
+import { facebook, instagram, shieldTick, support, truckFast, twitter } from "../assets/icons";
+import {
+  bigShoe1,
+  bigShoe2,
+  bigShoe3,
+  customer1,
+  customer2,
+  shoe4,
+  shoe5,
+  shoe6,
+  shoe7,
+  thumbnailShoe1,
+  thumbnailShoe2,
+  thumbnailShoe3,
+  tshirt1,
+  tshirt2,
+  tshirt3,
+  tshirt4,
+  thumbnail1,
+  thumbnail2,
+  thumbnail3,
+} from "../assets/images";
+
+export const navLinks = [
+    { href: "#home", label: "Home" },
+    { href: "#about-us", label: "About Us" },
+    { href: "#products", label: "Products" },
+    { href: "#contact-us", label: "Contact Us" },
+];
+
+export const shoes = [
+    {
+        thumbnail: thumbnailShoe1,
+        bigShoe: bigShoe1,
+    },
+    {
+        thumbnail: thumbnailShoe2,
+        bigShoe: bigShoe2,
+    },
+    {
+        thumbnail: thumbnailShoe3,
+        bigShoe: bigShoe3,
+    },
+];
+
+export const tshirts = [
+  {
+    thumbnail: thumbnail1,
+    tshirt: tshirt1,
+  },
+  {
+    thumbnail: thumbnail2,
+    tshirt: tshirt2,
+  },
+  {
+    thumbnail: thumbnail3,
+    tshirt: tshirt3,
+  },
+];
+
+export const statistics = [
+    { value: '1k+', label: 'Brands' },
+    { value: '500+', label: 'Shops' },
+    { value: '250k+', label: 'Customers' },
+];
+
+export const products = [
+  {
+    imgURL: tshirt1,
+    name: "Distrik Purple Doyang T-shirt",
+    price: "Rp 99.000",
+  },
+  {
+    imgURL: tshirt2,
+    name: "Distrik Beige Doyang T-shirt",
+    price: "Rp 99.000",
+  },
+  {
+    imgURL: tshirt3,
+    name: "Distrik Monas T-shirt",
+    price: "Rp 99.000",
+  },
+  {
+    imgURL: tshirt4,
+    name: "Distrik Keris Doyang T-shirt",
+    price: "Rp 99.000",
+  },
+];
+
+export const services = [
+    {
+        imgURL: truckFast,
+        label: "Free shipping",
+        subtext: "Enjoy seamless shopping with our complimentary shipping service."
+    },
+    {
+        imgURL: shieldTick,
+        label: "Secure Payment",
+        subtext: "Experience worry-free transactions with our secure payment options."
+    },
+    {
+        imgURL: support,
+        label: "Love to help you",
+        subtext: "Our dedicated team is here to assist you every step of the way."
+    },
+];
+
+export const reviews = [
+    {
+        imgURL: customer1,
+        customerName: 'Morich Brown',
+        rating: 4.5,
+        feedback: "The attention to detail and the quality of the product exceeded my expectations. Highly recommended!"
+    },
+    {
+        imgURL: customer2,
+        customerName: 'Lota Mongeskar',
+        rating: 4.5,
+        feedback: "The product not only met but exceeded my expectations. I'll definitely be a returning customer!"
+    }
+];
+
+
+export const footerLinks = [
+    {
+        title: "Products",
+        links: [
+            { name: "Distrik ", link: "/" },
+           
+        ],
+    },
+    {
+        title: "Help",
+        links: [
+            { name: "About us", link: "/" },
+            { name: "FAQs", link: "/" },
+            { name: "How it works", link: "/" },
+            { name: "Privacy policy", link: "/" },
+            { name: "Payment policy", link: "/" },
+        ],
+    },
+    {
+        title: "Get in touch",
+        links: [
+            { name: "customer@nike.com", link: "mailto:customer@nike.com" },
+            { name: "+92554862354", link: "tel:+92554862354" },
+        ],
+    },
+];
+
+export const socialMedia = [
+    { src: facebook, alt: "facebook logo" },
+    { src: twitter, alt: "twitter logo" },
+    { src: instagram, alt: "instagram logo" },
+];
